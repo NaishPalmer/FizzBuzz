2b1258009caa5957118c96f12137f90093f90d3d
Dexzify <dexzify@gmail.com>
2018-09-23 22:57:09 +0100

    First Commit

---
+++ Fizzbuzz/Fizzbuzz/PythonApplication1.py
@@ -0,0 +1,13 @@
+# This is a program to complete the Pro/g/ramming Challenges V1.4 by by /g/ on 4Chan.
+# This is a FizzBuzz Program
+
+for i in range(1, 100):
+	if i % 5 == 0 and i % 3 == 0: 
+		print('Fizzbuzz')
+	if i % 5 == 0:
+		print('Buzz')
+
+	elif i % 3 == 0:
+		print('Fizz')
+	else:
+		print(i)
